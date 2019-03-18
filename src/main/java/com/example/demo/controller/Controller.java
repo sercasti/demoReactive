79822c24f4b5d71cf23137b3be24b57825cb55db
sercasti <sercasti@gmail.com>
2019-03-18 20:25:47 -0300

    nonblocking operation

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,12 +1,6 @@
 package com.example.demo.controller;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
-import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Mono;
@@ -15,21 +9,10 @@ import reactor.core.publisher.Mono;
 public class Controller {
 
 	@GetMapping("/reactive")
-	public Mono<String> getMonoSha512Hex(@RequestParam String id) throws IOException {
-		String pathToFile = "/Users/sergio/Downloads/fotos/FOTO" + id + ".JPG";
-
-		return Mono.defer(() -> {
-			return Mono.fromCallable(() -> {
-				FileInputStream fileInputStream = null;
-				String sha512Hex = null;
-				try {
-					fileInputStream = new FileInputStream(new File(pathToFile));
-					sha512Hex = DigestUtils.sha512Hex(fileInputStream);
-				} finally {
-					fileInputStream.close();
-				}
-				return sha512Hex;
-			});
+	public Mono<String> getMonoSha512Hex() {
+		return Mono.fromCallable(() -> {
+			Pi.computePi(5000);
+			return "done";
 		});
 	}
 
